a8132b979740dbf6e897b69408831077bc854ce9
Tomasin0 <tomax12613@gmail.com>
2022-01-30 11:29:00 +0100

    zmiany

---
+++ ProjektASP/Models/TaskM8Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektASP.Models
+{
+    public class TaskM8Context : DbContext
+    {
+        public TaskM8Context(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<TaskModel> Tasks { get; set;  }
+        
+    }
+}
